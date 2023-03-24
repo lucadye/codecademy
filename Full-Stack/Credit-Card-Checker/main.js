5b3d3e96056331091445a47cbc71cf2cbe1da95a
lucadye <121118370+lucadye@users.noreply.github.com>
2023-03-24 10:38:39 -0400

    Credit Card Checker

---
+++ Full-Stack/Credit-Card-Checker/main.js
@@ -0,0 +1,85 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+
+
+// Add your functions below:
+
+function validateCred(card) {
+	let arr = [];
+	card.forEach(num => arr.push(num));
+	let bool = false
+	for (let i = arr.length - 1; i > -1; i--) {
+		if (bool) {
+			arr[i] *= 2;
+			if (arr[i] > 9) {
+				arr[i] -= 9;
+			}
+			bool = false
+		} else {
+			bool = true;
+		}
+	}
+	let sum = 0;
+	for (let i of arr) {
+		sum += i;
+	}
+	if (sum % 10 === 0) {
+		return true;
+	}
+	return false;
+}
+
+function findInvalidCards(_cards) {
+	let cards = [];
+	_cards.forEach(num => cards.push(num));
+	return cards.filter(card => validateCred(card));
+}
+
+function idInvalidCardCompanies(_cards) {
+	let companies = ['', '', '', '', ''];
+	let cards = [];
+	_cards.forEach(num => cards.push(num));
+	for (let card of cards) {
+		if (card[0] === 3) {
+			companies[0] = 'Amex (American Express)';
+		}
+		else if (card[0] === 4) {
+			companies[1] = 'Visa';
+		}
+		else if (card[0] === 5) {
+			companies[2] = 'Mastercard';
+		}
+		else if (card[0] === 6) {
+			companies[3] = 'Discover';
+		}
+		else {
+			companies[4] = 'Unknown';
+		}
+	}
+	companies.forEach(c => {
+		if (c != '') {
+			console.log(c);
+		}
+	});
+}
