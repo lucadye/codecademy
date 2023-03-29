508d2bb6db48ab11b8423b3e058e7ee8c8f6d412
lucadye <121118370+lucadye@users.noreply.github.com>
2023-03-29 10:24:23 -0400

    Complete minimum requirements for mixed messages

---
+++ Full-Stack/Mixed-Messages/main.js
@@ -0,0 +1,36 @@
+function rand_item(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const quote = {
+	_starts: [
+		'Remember,',
+		'Don\'t forget,',
+		'Tomorrow',
+		'Very soon'
+	],
+	_middles: [
+		'it\'ll all be',
+		'you\'ll feel',
+		'you\'ll see something',
+		'you\'ll find something'
+	],
+	_ends: [
+		'better.',
+		'new.',
+		'incredible.',
+		'wonderful.'
+	],
+	get new () {
+		const start = rand_item(this._starts);
+		const middle = rand_item(this._middles);
+		const end = rand_item(this._ends);
+		return `${start} ${middle} ${end}`;
+	},
+};
+
+function display(message) {
+	console.log(message);
+}
+
+display(quote.new);
